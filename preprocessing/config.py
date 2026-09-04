744f296162202886a4ce0a681b9761287b26f56a
Falah Naufal Zaki <falahnzk.dev@gmail.com>
2026-09-05 00:23:54 +0700

    better config for the lidar
    Signed-off-by: Falah Naufal Zaki <falahnzk.dev@gmail.com>

--- preprocessing/config.py
+++ preprocessing/config.py
@@ -4,7 +4,7 @@ import os
 from PIL import ImageFont
 
 class GlobalConfig:
-    datadir = "../datasetx/"
+    datadir = "../temp_datasetx/"
     cam_h = 128
     cam_w = 256
     lidbev_h = 256
@@ -64,7 +64,7 @@ class GlobalConfig:
     elif lidar_sensor == "rs32":
         v_fov = [-16, 15]
         dep_max = 150
-        v_res_div = 31
+        v_res_div = 60
     else: #"mid360" #rs32
         v_fov = [-15.5, 10.5] # HDL32 pakai [-30.67, 10.67], VLP32 pakai [-25, 15]
         dep_max = 200#/1.25 #dalam meter, baca datasheet np.sqrt(lid_cover_area_lr**2 + (cover_area_f[1]-cover_area_f[0])**2 + (lid_cover_area_bt[1]-((lid_cover_area_bt[1]-lid_cover_area_bt[0])/2))**2)

745353208f8bdbd5bd1473e05a6e4177173e5fe3
Falah Naufal Zaki <falahnzk.dev@gmail.com>
2026-09-07 20:50:54 +0700

    publish a single pcd to ros2 topic for visualization in rviz2
    Signed-off-by: Falah Naufal Zaki <falahnzk.dev@gmail.com>

---
+++ preprocessing/publish_lidar_ros2.py
@@ -0,0 +1,16 @@
+import rclpy
+from sensor_msgs.msg import PointCloud2
+from pypcd4 import PointCloud
+
+rclpy.init()
+node = rclpy.create_node('pcd_pub')
+pub = node.create_publisher(PointCloud2, '/pcd_points', 10)
+msg = PointCloud.from_path('empty_gap_front.pcd').to_msg()
+msg.header.frame_id = 'lidar'
+
+def pub_cb():
+    msg.header.stamp = node.get_clock().now().to_msg()
+    pub.publish(msg)
+
+node.create_timer(1.0, pub_cb)
+rclpy.spin(node)

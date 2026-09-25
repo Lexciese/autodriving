6f9de052a7cd714d45de8d996e3267af546795d6
Falah Naufal Zaki <falahnzk.dev@gmail.com>
2026-09-25 17:21:26 +0700

    use_tensor always True in process()

--- preprocessing/preprocessing_lidar.py
+++ preprocessing/preprocessing_lidar.py
@@ -97,7 +97,7 @@ class PreprocessingLidar(Preprocessing):
                 pty_ten = pcd_coords[:, 1]
                 ptz_ten = pcd_coords[:, 2]
 
-            self.bev_seg, self.bev_dep, self.front_seg, self.front_dep, self.rear_seg, self.rear_dep = self.gen_bev_front_rear_seg_dep(ptx_ten, pty_ten, ptz_ten, ptseg_ten, use_tensor=self.use_tensor)
+            self.bev_seg, self.bev_dep, self.front_seg, self.front_dep, self.rear_seg, self.rear_dep = self.gen_bev_front_rear_seg_dep(ptx_ten, pty_ten, ptz_ten, ptseg_ten, use_tensor=True)
 
             self.bev_segcol = colorize_seg(self.bev_seg.cpu().detach().numpy(), self.config.SEG_CLASSES['colors'])
             self.bev_depcol = colorize_logdepth(self.bev_dep.cpu().detach().numpy())
